2eb19800921fa7710c49c4190554814f4c5038ea
Johneryy <103750157+Johneryy@users.noreply.github.com>
2022-05-25 09:53:34 +0100

    Update DataBaseCreation.sql

--- DataBaseCreation.sql
+++ DataBaseCreation.sql
@@ -1,42 +1,42 @@
--- creating tables
+ creating tables
 
--- create table table_name(
+ create table table_name(
 
--- column_name datatype optional constraint,
--- column_name datatype optional constraint,
--- column_name datatype optional constraint,
+column_name datatype optional constraint,
+column_name datatype optional constraint,
+column_name datatype optional constraint,
 
--- .......
--- constraint constraint_name constraint_type(column_name)
--- );
-
-
-
--- use demo;
+.......
+constraint constraint_name constraint_type(column_name)
+);
 
 
--- create table Human(
--- humanID int(10) Not Null,
--- name varChar(30) Not Null,
--- age int(3) Not Null,
--- gender varChar(1) Not Null,
--- bloodType varChar(3) Not Null,
--- Genotype varChar(3) Not Null,
--- -- define the primary key column 
--- constraint human_PK  Primary Key(humanID)
--- );
 
--- -- Game table
--- create table Games (
--- GameID int(10) Not Null,
--- GameName varChar(30) Not Null,
--- Price numeric(4,2) Not Null,
--- Quantity int(10) Not Null,
--- Location varChar(25) Not Null,
--- constraint Games_PK Primary Key(gameID)
--- );
+use demo;
 
--- Game purchase table
+
+create table Human(
+humanID int(10) Not Null,
+name varChar(30) Not Null,
+age int(3) Not Null,
+gender varChar(1) Not Null,
+bloodType varChar(3) Not Null,
+Genotype varChar(3) Not Null,
+-- define the primary key column 
+constraint human_PK  Primary Key(humanID)
+);
+
+-- Game table
+create table Games (
+GameID int(10) Not Null,
+GameName varChar(30) Not Null,
+Price numeric(4,2) Not Null,
+Quantity int(10) Not Null,
+Location varChar(25) Not Null,
+constraint Games_PK Primary Key(gameID)
+);
+
+Game purchase table
 create table GamePurchase(
 humanID int(10) Not Null,
 GameID int(3) Not Null,
